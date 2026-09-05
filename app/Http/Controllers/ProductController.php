bdacd68a505fef5f4c07b1d2193ce8bdf82984fe
Christian Gales <etchangales@gmail.com>
2026-09-05 23:36:00 +0800

    fixed the few arguaments error

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -43,7 +43,7 @@ class ProductController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit()
     {
         //
         return view('products.edit-product');
